4b72e98cdbc67055ece09900d5ae5ad7feca304e
Ayan Ansari <1100zaid@gmail.com>
2021-10-06 12:42:43 +0530

    added new concepts

--- src/app.js
+++ src/app.js
@@ -44,46 +44,62 @@ const ProductList = new mongoose.model("ProductList", productListSchema);
 //   }
 // };
 // Todo:::: INserting many documents
-const createDocument = async () => {
-  try {
-    const onePlus = new ProductList({
-      name: "one plus",
-      productType: "Electronic",
-      Stock: 43,
-      COD: true,
-      active: true,
-    });
-    const herman = new ProductList({
-      name: "herman infinity",
-      productType: "Electronic",
-      Stock: 45,
-      COD: true,
-      active: true,
-    });
-    const boat = new ProductList({
-      name: "boat",
-      productType: "Electronic",
-      Stock: 12,
-      COD: false,
-      active: true,
-    });
-    const realme = new ProductList({
-      name: "realme",
-      productType: "Electronic",
-      Stock: 19,
-      COD: true,
-      active: true,
-    });
-    const result = await ProductList.insertMany([
-      onePlus,
-      herman,
-      boat,
-      realme,
-    ]);
-    console.log(result);
-  } catch (err) {
-    console.log(err);
-  }
-};
+// const createDocument = async () => {
+//   try {
+//     const onePlus = new ProductList({
+//       name: "one plus",
+//       productType: "Electronic",
+//       Stock: 43,
+//       COD: true,
+//       active: true,
+//     });
+//     const herman = new ProductList({
+//       name: "herman infinity",
+//       productType: "Electronic",
+//       Stock: 45,
+//       COD: true,
+//       active: true,
+//     });
+//     const boat = new ProductList({
+//       name: "boat",
+//       productType: "Electronic",
+//       Stock: 12,
+//       COD: false,
+//       active: true,
+//     });
+//     const realme = new ProductList({
+//       name: "realme",
+//       productType: "Electronic",
+//       Stock: 19,
+//       COD: true,
+//       active: true,
+//     });
+//     const result = await ProductList.insertMany([
+//       onePlus,
+//       herman,
+//       boat,
+//       realme,
+//     ]);
+//     console.log(result);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// createDocument();
+// Todo::: Reading the documents
+// const getDocument = async () => {
+// const result = await ProductList.find({Stock: 19}).select({name:1});      //* TO show only one info
+// console.log(result);
+// };
+// getDocument();
 
-createDocument();
+// Todo:: To update the document
+
+// const updateDocument = async () => {
+//   const result = await ProductList.updateOne(
+//     { name: "realme" },
+//     { $set: { name: "Realme Technologies" } }      //*updating only one value by using filter and then passing new value
+//   );
+//   console.log(result);
+// };
+// updateDocument();
